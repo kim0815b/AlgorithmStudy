b570acaa41a096a7586206246f6a24a305978ca8
wlsgus020815 <wlsgus020815@naver.com>
2025-07-15 14:46:46 +0900

    [solve]: 13023 ABCDE 문제해결

--- Jinhyeon/javaAlgo/src/Sol13023.java
+++ Jinhyeon/javaAlgo/src/Sol13023.java
@@ -7,26 +7,52 @@ import java.util.*;
 public class Sol13023 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static Map<String, List<String>> graph = new HashMap<>();
-	
-	public static void main(String[] args) {
-		String[] input = br.readLine().split();
+	static List<List<Integer>> graph = new ArrayList<>();
+	static int result;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException{
+		String[] input = br.readLine().split(" ");
 		int n = Integer.parseInt(input[0]);
 		int m = Integer.parseInt(input[1]);
-		for(int i; i < m; i++) {
+	    for (int i = 0; i < n; i++) {
+	    	graph.add(new ArrayList<>());
+	    }
+		for(int i=0; i < m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			String a = st.nextToken();
-			String b = st.nextToken();
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			addEdge(a, b);
 		}
+		visited = new boolean[n];
 		
+		for(int i = 0; i < n; i++) {
+			if (result != 1) {
+				visited[i] = true;
+				dfs(i, 1);
+				visited[i] = false;
+			}
+		}
+		
+		bw.write(String.valueOf(result));
+		bw.flush();
 	}
-	static void dfs(String node, int depth) {
-		
+	
+	static void dfs(int node, int depth) {
+		if (depth > 4) {
+			result = 1;
+			return;
+		}
+		for(int n: graph.get(node)) {
+			if (!visited[n]){
+				visited[n] = true;
+				dfs(n, depth + 1);
+				visited[n] = false;
+			}
+		}
 	}
 	
 	
-	static void addEdge(String a, String b) {
+	static void addEdge(int a, int b) {
 		graph.get(a).add(b);
 		graph.get(b).add(a);
 	}
